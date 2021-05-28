5598bae5a95764bc6824b6f79bdbb2caa9ac9ff1
Toppedyk <Tayloroppedyk@yahoo.com>
2021-05-28 10:58:35 -0600

    project startup

---
+++ Keepr.Client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = 'taylor-oppedyk.us.auth0.com'
+export const audience = 'https://keepr.com'
+export const clientId = 'FBW8uVQ5Ma5D7ExQNLNGOcjUakBqiQRN'
