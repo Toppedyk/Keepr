1c50d4c230a948fd774cc7e20c189deb4fbdc7e5
Toppedyk <Tayloroppedyk@yahoo.com>
2021-05-30 12:23:09 -0600

    progress on keep card

--- Keepr.Client/src/components/Keep.vue
+++ Keepr.Client/src/components/Keep.vue
@@ -1,11 +1,28 @@
 <template>
-  <div class="Keep">
+  <div class="Keep col-3">
+    <div class="card d-flex" :style="{ backgroundImage: `url(${keep.img})` }">
+      <!-- <img :src="keep.img" class="card-img-top" alt="..."> -->
+      <div class="card-body">
+        <h5 class="card-title">
+          Card title
+        </h5>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Keep',
+  props: {
+    keep: {
+      type: Object,
+      required: true
+    }
+  },
   setup() {
     return {}
   },
@@ -14,5 +31,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.card{
+background-size: contain;
+}
 
 </style>
